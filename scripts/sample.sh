4630dad2396f83b2418d5bbca15894180402c496
Eri Bastos <eri.bastos@corp.eastlink.ca>
2018-03-06 12:16:40 -0400

    Fixed issue #1: Will remove temp file if gets interrupted

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Hello from Shell Script! I got $@"
+echo "Hello from Shell Script! I am $0"
+echo -n "Sleeping for 30 seconds"
+for i in {1..30}
+do
+    sleep 1
+    echo -n "."
+done
+echo "[DONE]"
